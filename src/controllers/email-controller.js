3aa4cea892bebb149fd1fcd00f450fa511982b0d
rayan1242 <rayyanmaindargi12@gmail.com>
2023-09-01 21:06:44 +0530

    FEAT: Added pub-sub queue between noti-service and booking service

--- src/controllers/email-controller.js
+++ src/controllers/email-controller.js
@@ -10,7 +10,7 @@ async function create(req,res) {
         return res.status(201).json(response);
     } catch (error) {
         return res.status(500).json(error);
-    }
+    } 
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
  const express = require ('express');
-
+ const amqplib = require('amqplib')
+ const {EmailService} = require('./services');
  const{ ServerConfig, Logger } = require('./config');
 const apiRoutes = require('./routes');
 const mailsender = require('./config/email-config');
  const app = express();
  
+ async function connectQueue(){
+  try {
+    const connection = await amqplib.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+    await channel.assertQueue('noti-queue');
+   channel.consume('noti-queue', async(data) =>{ 
+                  const object = JSON.parse(`${Buffer.from(data.content)}`);
+                  await EmailService.sendEmail(ServerConfig.GMAIL_EMAIL,object.recepientEmail,object.subject,object.text);
+                  channel.ack(data);
+                  });
+  } catch (error) {
+    console.log(error);
+  }
+} 
+
  
  app.use(express.json());
  app.use(express.urlencoded({extended:true}));
@@ -13,8 +29,9 @@ const mailsender = require('./config/email-config');
 
 app.use('/api',apiRoutes);
 
- app.listen(ServerConfig.PORT,async () =>{
+ app.listen(ServerConfig.PORT, () =>{
    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
    Logger.info("Successfully started the server",{});
+    connectQueue();
 })
  
